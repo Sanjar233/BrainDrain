63f4ecf60e1c36f4bed888408705fa3d026b6c9d
sanzharMinbaev <sanman233@gmail.com>
2024-04-03 22:41:57 +0600

    initial commit

---
+++ src/main/java/com/example/BrainDrain/BrainDrainApplication.java
@@ -0,0 +1,13 @@
+package com.example.BrainDrain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrainDrainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrainDrainApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BrainDrain/BrainDrainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BrainDrain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrainDrainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
